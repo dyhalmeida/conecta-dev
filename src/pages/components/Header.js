2e58b029182bd7f02066b5583972d34dbe9031a5
Diego Almeida <dyhalmeida@gmail.com>
2021-02-01 08:04:56 -0300

    :sparkles: Created Header

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,18 +1,62 @@
 import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { Bell } from 'react-feather';
+
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+    appBar: {
+        boxShadow: 'none'
+    },
+    logo: {
+        maxHeight: 55,
+    },
+    grow: {
+        flexGrow: 1
+    }, 
+    userSection: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    button: {
+        marginRight: 10
+    },
+    bell: {
+        marginRight: 10
+    }
+})
 
 export default function Header(props) {
+    const style = useStyles();
     return (
-        <header className="header">
-            <div className="toolbar">
-                <div className="">
-                    <span>Conecta-dev</span>
+        <AppBar position='fixed' color='inherit' className={style.appBar}>
+            <Toolbar>
+
+                <img className={style.logo} src="/images/logo.png" alt="Conecta Dev"></img>
+                <div className={style.grow}></div>
+                <div className={style.userSection}>
+                    <Button variant="contained" color="primary" className={style.button}>
+                        New Post
+                    </Button>
+                    <SvgIcon className={style.bell}>
+                        <Bell></Bell>
+                    </SvgIcon>
+                    <Avatar alt="Diego Almeida" src="/" />
+                </div>
+
+                {/* <div className="">
+                    <a href="/">Conecta-dev</a>
+                    <input></input>
                 </div>
                 <div className="">
-                    <button>New Post</button>
+                   
                     <span>img1</span>
                     <span>img2</span>
-                </div>
-            </div>
-        </header>
+                </div> */}
+            </Toolbar>
+        </AppBar>
     )
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Header from '../components/Header';
+import Feed from '../components/Feed';
+import NavBar from '../components/NavBar';
 
-import './index.css';
+import { makeStyles } from '@material-ui/core/styles';
+// import './index.css';
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    main: {
+        height: '100vh',
+        display: 'flex',
+        width: '1200px',
+        margin: '0 auto',
+    }
+});
 
 export default function Home() {
+    const style = useStyles();
     return (
-        <div>
+        <div className={style.root}>
             <Header />
-            <main className="main">
-                <div className="navbar">Navbar</div>
-                <div className="feed">Feed</div>
+            <main className={style.main}>
+                <NavBar />
+                <Feed />
             </main>
         </div>
     )
