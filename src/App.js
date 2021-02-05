0828455f1845c9a77bae668cf1c11029fc3c309a
Diego Almeida <dyhalmeida@gmail.com>
2021-02-05 15:22:40 -0300

    :sparkles: add theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Home from './pages/home'
 import './index.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 function App() {
-  return <Home />
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme/palette.js
@@ -0,0 +1,10 @@
+import { colors } from '@material-ui/core';
+
+export default {
+    primary: {
+        main: colors.indigo['A400']
+    },
+    secondary: {
+        main: colors.orange['A700']
+    },
+}
